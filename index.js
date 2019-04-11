cada9548298e3d94ce88e1b675dab744d09a590d
brandiw <brandimichelle.0911@gmail.com>
2019-04-11 11:33:19 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Include .env variables
+
+// Require necessary modules
+
+// Declare Express app
+
+// Set view engine
+
+// Include (use) middle ware
+
+// Include routes from controllers
+
+// Make a home route: GET /
+
+// Listen from your port
