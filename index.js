782d714c5522229cca3bbf88b2ca10fe543d29a3
brandiw <brandimichelle.0911@gmail.com>
2019-04-11 11:47:56 -0700

    Working Express app. Home page stubbed

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // Include .env variables
+require('dotenv').config()
 
 // Require necessary modules
+let express = require('express')
 
 // Declare Express app
+let app = express()
 
 // Set view engine
+app.set('view engine', 'ejs')
 
 // Include (use) middle ware
 
 // Include routes from controllers
 
 // Make a home route: GET /
+app.get('/', (req, res) => {
+  res.send('<h1>STUB</h1>')
+})
 
 // Listen from your port
+app.listen(process.env.PORT || 3000)
