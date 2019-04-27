bd835996725b3c963a342b85b74f0f64f13ba43b
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-04-27 12:37:15 -0700

    updated scrapemoves.js with correct(?) async.mapSeries function. need to troubleshoot 'SyntaxError: Unexpected identifier' regarding line with text: import mapSeries from 'async/mapSeries';

--- scrapemoves.js
+++ scrapemoves.js
@@ -2,10 +2,12 @@ const express = require('express')
 const request = require('request');
 const cheerio = require('cheerio');
 const async = require('async');
-
+import mapSeries from 'async/mapSeries';
+import doLimit from './internal/doLimit';
 
 
 let moveArr = [];
+let moveUrlArr = [];
 let movesObjList = [];
 
 
@@ -17,29 +19,37 @@ function generateMovesObjList() {
 
     request(`https://www.serebii.net/attackdex-xy/absorb.shtml`, (err, cheerioResp, html) => {
       if (!err && cheerioResp.statusCode == 200) {
-        const $ = cheerio.load(html)
-
-        let moveName = $('select').eq(0).children('option').eq(1).text();
+        const $ = cheerio.load(html);
 
         for (var j = 0; j < $('select').length; j++) {
-          for (var i = 1; i < $('select').eq(0).children('option').length; i++) {
-            let moveName = $('select').eq(j).children('option').eq(i).text();
-            moveArr.push(moveName)
+          for (var i = 1; i < $('select').eq(j).children('option').length; i++) {
+
+            // properly capitalized name
+            let moveName = $('select').eq(j).children('option').eq(i).text().toLowerCase();
+            moveArr.push(moveName);
+
+            // mutations to find URL-appropriate string
+            let moveNameR = moveName.match(/(\w+['-]?)/g);
+            let resultLowerCommas = moveNameR.join();
+            let moveNameUrl = resultLowerCommas.replace(/,/g, '');
+            // console.log(`moveNameUrl = ${moveNameUrl}`);
+            moveUrlArr.push(moveNameUrl);
+
           }
         }
-        let dropbox1length = $('select').length
-        //.eq(0).children('option').length
 
-        console.log(moveArr);
+
+        // console.log(`moveArr = ${moveArr}`);
 
 
         moveArr.forEach((move, i) => {
           let newMoveObj = {
             id: i + 1,
-            name: move
+            name: move,
+            urlName: moveUrlArr[i]
           }
           movesObjList.push(newMoveObj);
-        })
+        });
 
         resolve(movesObjList);
 
@@ -58,7 +68,7 @@ async function asyncMapRequests() {
 
 
 
-  async.map(movesObjList, asyncfunc, (err, results) => {
+  async.mapSeries(movesObjList, asyncfunc, (err, results) => {
     console.log(results)
   });
 
@@ -124,3 +134,4 @@ async function asyncMapRequests() {
 }
 
 asyncMapRequests();
+export default doLimit(mapLimit, 1);
