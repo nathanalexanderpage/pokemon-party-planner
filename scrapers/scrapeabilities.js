b69cb62b7e9093c5e00b59d87e7d523d0ec0e7ac
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-08-12 13:10:40 -0700

    added heroku scraper troubleshoot code in scrapeabilities.js

--- scrapers/scrapeabilities.js
+++ scrapers/scrapeabilities.js
@@ -111,7 +111,7 @@ async function asyncMapRequests() {
         let desc = $('.dextable').eq(1).children('tbody').children('tr').eq(3).text();
         let descRegex = desc.match(/([é\w]+[-'!,.\?\s])+/g).join(' ');
 
-        let tempVar = Number($('[name=pkmn]').parent().next().children('table').children('tbody').children('tr').eq(2).children('td').eq(0).text();
+        let tempVar = Number($('[name=pkmn]').parent().next().children('table').children('tbody').children('tr').eq(2).children('td').eq(0).text());
 
         console.log(tempVar);
 
