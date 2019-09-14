7a647b450558797cb63fd9592728174e92a29a83
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-09-13 19:29:00 -0700

    co-occurrence SQL command string interpolated for reusability

--- recommend_engine.py
+++ recommend_engine.py
@@ -1,5 +1,5 @@
-print("recommend_engine.py RUNNING")
-print("recommend_engine.py IMPORTING necessary modules")
+print("recommend_engine.py ||| RUNNING")
+print("recommend_engine.py ||| IMPORTING necessary modules")
 
 import numpy as np
 import os
@@ -9,20 +9,44 @@ import psycopg2
 from dotenv import load_dotenv
 from sklearn.metrics.pairwise import pairwise_distances
 
-print("recommend_engine.py INITIALIZING .env variables")
+print("recommend_engine.py ||| INITIALIZING .env variables")
 load_dotenv()
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 
-print("recommend_engine.py CONNECTING to database")
-# establish connection to database
-conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER)
-# create cursor object, used to execute SQL statements
-cur = conn.cursor()
+def query_cooccurrences(search_id=17):
+    """ PURPOSE: grab list of tuples from which to generate co-occurrence matrix """
 
-print("recommend_engine.py CLOSING database connection")
-# finish by closing connections
-cur.close()
-conn.close()
+    print("recommend_engine.py ||| CONNECTING to database")
+    # establish connection to database
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER)
+    # create cursor object, used to execute SQL statements
+    cur = conn.cursor()
 
-print("recommend_engine.py DONE")
+    search_input_id = str(search_id)
+
+    sql_query = """
+        SELECT op.\"partyId\", o.\"dexId\"
+        FROM owns_parties op
+        INNER JOIN owns o ON o.id = op.\"ownId\"
+        WHERE \"partyId\" IN
+            (SELECT DISTINCT \"partyId\" FROM owns_parties WHERE \"ownId\" IN
+                (SELECT id FROM owns WHERE \"dexId\" = {id_to_search})
+            )
+        ORDER BY \"partyId\", \"dexId\" ASC;
+        """.format(id_to_search=search_input_id)
+
+    print("recommend_engine.py ||| EXECUTING SQL", sql_query)
+    cur.execute(sql_query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    print("recommend_engine.py ||| CLOSING database connection")
+    # finish by closing connections
+    cur.close()
+    conn.close()
+
+query_cooccurrences(17)
+
+print("recommend_engine.py ||| DONE")
